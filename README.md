081067080622da6f6c7c111cddbe9bbe88a9b6bf
Anbiya Popal <55081188+itscountvertigo@users.noreply.github.com>
2021-02-10 14:58:54 +0100

    code now outputs both players' new rating / added demo image

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This is a simple Elo rating system to calculate player rating after a match. Elo
 About the system: https://en.wikipedia.org/wiki/Elo_rating_system
 
 I didn't make these formulas up, I only turned it into a python script.
+
+![demo image](demo.png)
--- elo.py
+++ elo.py
@@ -1,23 +1,33 @@
-ratingA = int(input("What is your rating? "))
-ratingB = int(input("what is your opponent's rating? "))
-winner = input("who won? input 'win' if you won, 'lose' if you lost. ")
+rating_a = int(input("What is your rating?\n"))
+rating_b = int(input("what is your opponent's rating?\n"))
+winner = input("who won? input 'win' if you won, 'lose' if you lost.\n")
 
 if winner == "win":
-    winner = 1
+    winner_a = 1
+    winner_b = 0
 elif winner == "lose":
-    winner = 0
+    winner_a = 0
+    winner_b = 1
 
-probability = 1 / (1 + 10 ** ((ratingB - ratingA) / 400))
+probability_a = 1 / (1 + 10 ** ((rating_b - rating_a) / 400))
+probability_b = 1 - probability_a
 
-print("probability of you winning: " + str(round(probability, 3)))
+# print("probability of you winning: " + str(round(probability_a, 3)))
+# print("probability of your opponent winning: " + str(round(probability_b, 3)))
 
-newRatingA = ratingA + 32 * (winner - probability)
+new_rating_a = rating_a + 64 * (winner_a - probability_a)
+new_rating_b = rating_b +64 * (winner_b - probability_b)
 
-diff = round(newRatingA) - ratingA
+diff_a = round(new_rating_a) - rating_a
+diff_b = round(new_rating_b) - rating_b
 
-if diff > 0:
-    diff = "+" + str(diff)
-else:
-    diff = str(diff)
 
-print("Your new rating: " + str(round(newRatingA)) + ", " + diff)
+if diff_a > 0:
+    diff_a = "+" + str(diff_a)
+
+if diff_b > 0:
+    diff_b = "+" + str(diff_b)
+
+
+print("Your new rating: " + str(round(new_rating_a)) + ", " + str(diff_a))
+print("Your opponent's new rating: " + str(round(new_rating_b)) + ", " + str(diff_b))
